59430b66371037894ba7f129c7037a878e143d69
C. Felix Lee <felixlee.music@gmail.com>
2023-07-26 01:45:06 -0400

    1st api route created: GET by username

--- db.js
+++ db.js
@@ -18,6 +18,20 @@ const User = conn.define('user', {
     allowNull: false,
     unique: true,
   },
+  password: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
 });
 
 const Course = conn.define('course', {
@@ -92,13 +106,23 @@ Topic.belongsTo(Course);
 Course.hasMany(Topic);
 File.belongsTo(Topic);
 Topic.hasMany(File);
+File.belongsTo(User);
+User.hasMany(File);
+File.belongsTo(Course);
+Course.hasMany(File);
 
 // SEEDING DATA
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
   // user(s)
   const [felix] = await Promise.all(
-    ['felix'].map((name) => User.create({ username: name }))
+    ['felix'].map((name) =>
+      User.create({
+        username: name,
+        password: '789',
+        email: 'felixlee.music@gmail.com',
+      })
+    )
   );
 
   // course(s)
@@ -119,32 +143,33 @@ const syncAndSeed = async () => {
     File.create({
       userId: felix.id,
       courseId: bootcamp.id,
-      mediaUrl: 'static/img/CreateCalculator.png',
+      mediaUrl: '/img/CreateCalculator.png',
     }),
     File.create({
       userId: felix.id,
       courseId: bootcamp.id,
       topicId: dsa.id,
-      mediaUrl: 'static/img/Soup Solution 1st Checkpoint.png',
+      mediaUrl: '/img/soupSolution.png',
       description:
         "some algorithm I don't remember, hence why I need this app!",
     }),
     File.create({
       userId: felix.id,
       courseId: bootcamp.id,
-      mediaUrl: 'static/img/hasCoworker.png',
+      mediaUrl: '/img/hasCoworker.png',
       description: 'working w/ objects',
     }),
   ]);
 
-  console.log(felix.get());
-  console.log(bootcamp.get());
-  console.log(soup.get());
-  console.log(hasCoworker.get());
+  console.log('user: ', felix.get());
+  console.log('course: ', bootcamp.get());
+  console.log('file1: ', soup.get());
+  console.log('file2: ', hasCoworker.get());
 };
 
 module.exports = {
   syncAndSeed,
+  conn,
   models: {
     User,
     Course,
--- server.js
+++ server.js
@@ -1,9 +1,53 @@
-const { syncAndSeed } = require("./db");
+const {
+  syncAndSeed,
+  conn,
+  models: { User, Course, Topic, File },
+} = require('./db');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/api/:username', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.params.username },
+      include: [{ model: Course }],
+    });
+    res.send(user);
+  } catch (ex) {
+    next(ex);
+  }
+});
+app.get('/', async (req, res, next) => {
+  try {
+    const [users, courses, topics, files] = await Promise.all([
+      User.findAll(),
+      Course.findAll({
+        include: [File],
+      }),
+      Topic.findAll(),
+      File.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+        ],
+      }),
+    ]);
+    res.send();
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 const init = async () => {
   try {
-    console.log("app ready");
     await syncAndSeed();
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`listening on port ${port}`));
   } catch (er) {
     console.log(er);
   }
